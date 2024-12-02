6e251fb8fc6d353e0a1f0e69d8cc4b514b80910b
michal <michalsuhak@bluerider.software>
2024-12-02 20:14:34 +0100

    cloud watch agent config

--- infrastructure/terraform/app-bootstrap-script.sh
+++ infrastructure/terraform/app-bootstrap-script.sh
@@ -19,7 +19,19 @@ echo "app/" >> .git/info/sparse-checkout
 git pull origin main
 cd app || exit
 
+# Create FastAPI logs directory
+mkdir -p /var/log/fastapi
+touch /var/log/fastapi/fastapi.log
+chmod 644 /var/log/fastapi/fastapi.log
+
+# Run CloudWatch agent
+# Configuration can be stored in the parameter store I use file due to financial considerations
+/opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+  -a fetch-config \
+  -m ec2 \
+  -c file:/app/app/cloud-watch-agent-config.json \
+  -s
 
 # Install dependencies and run app
 poetry install
-poetry run fastapi run app.py
+poetry uvicorn app.app:app --host 0.0.0.0 --port 8000 --reload
